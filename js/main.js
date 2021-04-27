896eaf059809d7b1046b982a79d1fe0889803b47
Dasha <dasha@100up.ru>
2021-04-27 12:33:58 +0300

    fix first dz (module 3.1) add second dz (module 3.2)

--- js/main.js
+++ js/main.js
@@ -1,44 +1,84 @@
 'use strict';
 
-(function() {
-  function randomizer(min, max) {
+(() => {
+  const randomizer = (min, max) => {
     // получить случайное число от (min-0.5) до (max+0.5)
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
-  }
-  let array = [];
-  let names = ['Святогор', 'Пафнутий', 'Тамара', 'Василиса', 'Радриго', 'Ярило'];
-  let messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  let randomComments = [];
-  for (let x = 0; x < randomizer(2,6); x++) {
-    randomComments[x] = {
-      avatar: `img/avatar-${randomizer(1, 6)}.svg`,
-      message: `${messages[randomizer(0, messages.length)]}`,
-      name: `${names[randomizer(0, names.length)]}`
-    }
-  }
-  for (let i = 0; i < 25; i++) {
-    array[i] = {
-      url: `photos/${i}.svg`,
+  };
+  const names = ['Святогор', 'Пафнутий', 'Тамара', 'Василиса', 'Радриго', 'Ярило'];
+  const messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
+  const randomComments = () => {
+    return Array.from({length: randomizer(2,6)}, () => {
+      return {
+        avatar: `img/avatar-${randomizer(1, 6)}.svg`,
+        message: `${messages[randomizer(0, messages.length - 1)]}`,
+        name: `${names[randomizer(0, names.length -1)]}`
+      }
+    });
+  };
+
+  const array = Array.from({length: 25}, (item, i) => {
+    return {
+      url: `photos/${i+1}.svg`,
       description: 'Описание',
       likes: randomizer(15, 200),
-      comments: randomComments
+      comments: randomComments()
     };
+  });
 
-  }
   const picWrapper = document.querySelector('.pictures');
   const picFragment = new DocumentFragment();
   const picTemplate = document.querySelector('#picture').content;
-  for (let j = 0; j < array.length; j++){
+  array.forEach(item => {
     const newPic = picTemplate.querySelector('.picture').cloneNode(true);
     const img = newPic.querySelector('.picture__img');
     const likes = newPic.querySelector('.picture__likes');
     const comments = newPic.querySelector('.picture__comments');
-    img.src = array[j].url;
-    likes.textContent = array[j].likes;
-    comments.textContent = array[j].comments.length;
-    console.log(newPic);
+    img.src = item.url;
+    likes.textContent = item.likes;
+    comments.textContent = item.comments.length;
     picFragment.append(newPic);
-  }
+  });
   picWrapper.append(picFragment);
+
+  // DZ 2
+  const bigPic = document.querySelector('.big-picture');
+  const bigPicImg = bigPic.querySelector('.big-picture__img img');
+  const bigPicLike = bigPic.querySelector('.likes-count');
+  const bigPicCommentsCount = bigPic.querySelector('.comments-count');
+  const bigPicComments = bigPic.querySelector('.social__comments');
+  const bigPicDescription = bigPic.querySelector('.social__caption');
+  const bigPicCommentFragment = new DocumentFragment();
+
+  bigPicImg.src = array[0].url;
+  bigPicLike.textContent = array[0].likes;
+  bigPicCommentsCount.textContent = array[0].comments.length;
+  bigPicDescription.textContent =  array[0].description;
+
+  array[0].comments.forEach(item => {
+    const bigPicComment = bigPic.querySelector('.social__comment').cloneNode(true);
+    const bigPicCommentsImg = bigPicComments.querySelector('img');
+    const bigPicCommentsText = bigPicComments.querySelector('.social__text');
+    bigPicCommentsImg.src = item.avatar;
+    bigPicCommentsImg.alt = item.name;
+    bigPicCommentsText.textContent = item.message;
+    bigPicCommentFragment.append(bigPicComment);
+  });
+
+  while (bigPicComments.firstChild) {
+    bigPicComments.removeChild(bigPicComments.firstChild);
+  }
+  bigPicComments.append(bigPicCommentFragment);
+
+  const openBigImg = () => {
+    bigPic.classList.remove('hidden');
+    const commentsCount = document.querySelector('.social__comment-count');
+    const commentsLoader = document.querySelector('.comments-loader');
+    commentsCount.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+    document.querySelector('body').classList.add('modal-open');
+  };
+  openBigImg();
 })();
