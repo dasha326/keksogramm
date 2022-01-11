438563092f5571aa1e4ec46bd02d878b30586718
Dasha <dasha@100up.ru>
2022-01-11 17:58:18 +0300

    add module 5.1

---
+++ js/gallery.js
@@ -0,0 +1,24 @@
+import {utils} from './utils.js';
+import {preview} from './preview.js';
+export const gallery = (images) =>{
+  const picWrapper = document.querySelector('.pictures');
+  const picFragment = new DocumentFragment();
+  const picTemplate = document.querySelector('#picture').content;
+  images.forEach(item => {
+    const newPic = picTemplate.querySelector('.picture').cloneNode(true);
+    const img = newPic.querySelector('.picture__img');
+    const likes = newPic.querySelector('.picture__likes');
+    const comments = newPic.querySelector('.picture__comments');
+    img.src = item.url;
+    likes.textContent = item.likes;
+    comments.textContent = item.comments.length;
+    newPic.addEventListener('click', (e)=>{
+      e.preventDefault();
+      e.stopPropagation();
+      preview(item.url, item.likes, item.description, item.comments);
+    });
+    picFragment.append(newPic);
+  });
+  picWrapper.append(picFragment);
+
+};
---
+++ js/preview.js
@@ -0,0 +1,47 @@
+import {utils} from './utils.js';
+
+const bigPic = document.querySelector('.big-picture');
+const bigPicImg = bigPic.querySelector('.big-picture__img img');
+const bigPicClose = bigPic.querySelector('.big-picture__cancel');
+const bigPicLike = bigPic.querySelector('.likes-count');
+const bigPicCommentInput = bigPic.querySelector('.social__footer-text');
+const bigPicCommentsCount = bigPic.querySelector('.comments-count');
+const bigPicComments = bigPic.querySelector('.social__comments');
+const bigPicDescription = bigPic.querySelector('.social__caption');
+const bigPicCommentFragment = new DocumentFragment();
+
+export const preview = (imgUrl, likeCount, description, comments) =>{
+  bigPicImg.src = imgUrl;
+  bigPicLike.textContent = likeCount;
+  bigPicDescription.textContent = description;
+  bigPicCommentsCount.textContent = comments.length;
+  utils.openModal(bigPic);
+
+  // Добавление коментов
+  comments.forEach(item => {
+    const bigPicComment = bigPic.querySelector('.social__comment').cloneNode(true);
+    const bigPicCommentsImg = bigPicComments.querySelector('img');
+    const bigPicCommentsText = bigPicComments.querySelector('.social__text');
+    bigPicCommentsImg.src = item.avatar;
+    bigPicCommentsImg.alt = item.name;
+    bigPicCommentsText.textContent = item.message;
+    bigPicCommentFragment.append(bigPicComment);
+  });
+  bigPicComments.append(bigPicCommentFragment);
+  //const commentsCount = document.querySelector('.social__comment-count');
+  //const commentsLoader = document.querySelector('.comments-loader');
+  //commentsCount.classList.add('hidden');
+  //commentsLoader.classList.add('hidden');
+
+  //Обработчик закрытия
+  bigPicClose.addEventListener('click', () => {
+    utils.closeModal(bigPic);
+  });
+  document.addEventListener('keydown', e => {
+    utils.isEscEvent(e, () => {
+      if (document.activeElement !== bigPicCommentInput) {
+        utils.closeModal(bigPic);
+      }
+    });
+  });
+};
